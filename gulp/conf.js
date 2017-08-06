6bd4f436f43b248301dbd375dff3a45a03db03a3
Frank Fox <ffox77@yahoo.com>
2017-08-06 08:46:27 -0700

    publish for gh-pages

--- gulp/conf.js
+++ gulp/conf.js
@@ -13,7 +13,7 @@ var gutil = require('gulp-util');
  */
 exports.paths = {
     src: 'src',
-    dist: 'dist',
+    dist: 'docs',
     assets: 'src/assets',
     builds: 'src/builds',
     tmp: '.tmp',
