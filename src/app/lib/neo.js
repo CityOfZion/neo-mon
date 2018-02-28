e9d48f504dd9142f773c8fe21ec4aba2b0ee3a05
Frank Fox <ffox77@yahoo.com>
2018-02-28 10:13:56 -0800

    Added latency in dashboard for RPC nodes. Added new icon flags for India and Japan. Had to temporarily remove metachris'  REST endpoints  pyapi as this requires a custom service handler in neo-api-js

--- src/app/lib/neo.js
+++ src/app/lib/neo.js
@@ -22,19 +22,35 @@ function getProtocolClient () {
 function serviceOptions(service, serviceName, initObj) {
 
     if (typeof initObj === 'string') {
-        initObj = { baseUrl: initObj};
+        initObj = { baseUrl: initObj };
     }
     else if (typeof initObj !== 'object') {
         initObj = {};
     }
 
+    service.serviceLatency = 0;
+    service.serviceLatencyStartTime = 0;
     service.serviceName = serviceName;
     service.serviceBaseUrl = initObj.baseUrl || '';
     service.servicePollInterval = initObj.poll;
+    service.serviceMonitorLatency = initObj.monitorLatency;
 
     service.baseUrl = baseUrl;
     service.protocolClient = protocolClient;
     service.poll = poll;
+    service.monitorLatency = monitorLatency;
+    service.startLatencyTimer = startLatencyTimer;
+    service.stopLatencyTimer = stopLatencyTimer;
+    service.latency = latency;
+
+
+    function startLatencyTimer () {
+        service.serviceLatencyStartTime = Date.now();
+    }
+
+    function stopLatencyTimer () {
+        service.serviceLatency = Date.now() - service.serviceLatencyStartTime;
+    }
 
     function baseUrl (val) {
 
@@ -65,6 +81,29 @@ function serviceOptions(service, serviceName, initObj) {
         }
 
         this.servicePollInterval = val;
+
+        return this;
+    }
+
+    function monitorLatency (val) {
+
+        if (!val) {
+            return this.serviceMonitorLatency;
+        }
+
+        this.serviceMonitorLatency = val;
+
+        return this;
+    }
+
+    function latency (val) {
+
+        if (!val) {
+            return this.serviceLatency;
+        }
+
+        //read-only
+        //this.serviceLatency = val;
 
         return this;
     }
@@ -371,6 +410,11 @@ function makeServiceRequest (restService, httpOptions) {
 
         var poll = restService.poll();
 
+        if (restService.monitorLatency()) {
+            ctx.startLatencyTimer = restService.startLatencyTimer;
+            ctx.stopLatencyTimer = restService.stopLatencyTimer;
+        }
+
         if (poll) {
             var pollRunner = service.getPollRunner(poll).addRequest(function () {
                 return _makeServiceRequest(client, options, ctx);
@@ -424,23 +468,31 @@ function _wrapPromise (callback) {
 }
 
 function prepareContext() {
-    var ctx = {};
+    var ctx = { };
 
     ctx.stopPolling = noop;
-    ctx.isPolling = function () { return false; };
+    ctx.isPolling = noop;//function () { return false; };
+    ctx.startLatencyTimer = noop;
+    ctx.stopLatencyTimer = noop;
 
     return ctx;
 }
 
 function _makeServiceRequest (client, options, ctx) {
 
+    ctx.startLatencyTimer();
+
     var promise = client.invoke(options);
 
     promise.catch(function (response) {
         ctx.errorFunction(response);
+
+        ctx.stopLatencyTimer();
     });
 
     promise = promise.then(function (response) {
+
+        ctx.stopLatencyTimer();
 
         var data = ctx.transformResponse(response);
 
@@ -656,65 +708,177 @@ function node(options) {
 
     serviceOptions(inst, 'node', options);
 
-    //Asset
+    inst.dumpPrivKey = dumpPrivKey;
+    inst.getAccountState = getAccountState;
+    inst.getApplicationLog = getApplicationLog;
+    inst.getAssetState = getAssetState;
     inst.getBalance = getBalance;
-
-    //Block
-    inst.getLastBlockHash = getLastBlockHash;
-    inst.getBlockByHeight = getBlockByHeight$1;
+    inst.getBestBlockHash = getBestBlockHash;
+    inst.getBlock = getBlock;
     inst.getBlockCount = getBlockCount;
-    inst.getBlockHashByHeight = getBlockHashByHeight;
-
-    //Net
+    inst.getBlockHash = getBlockHash;
+    inst.getBlockSysFee = getBlockSysFee;
     inst.getConnectionCount = getConnectionCount;
-    inst.getVersion = getVersion;
-
-    //Tx
+    inst.getContractState = getContractState;
+    inst.getNewAddress = getNewAddress;
     inst.getRawMemPool = getRawMemPool;
     inst.getRawTransaction = getRawTransaction;
+    inst.getStorage = getStorage;
     inst.getTxOut = getTxOut;
+    inst.getPeers = getPeers;
+    inst.getVersion = getVersion;
+    inst.invoke = invoke;
+    inst.invokeFunction = invokeFunction;
+    inst.invokeScript = invokeScript;
+    inst.listAddress = listAddress;
+    inst.sendRawTransaction = sendRawTransaction;
+    inst.sendToAddress = sendToAddress;
+    inst.sendMany = sendMany;
+    inst.validateAddress = validateAddress;
+
 
     return inst;
 }
 
+
+//http://docs.neo.org/en-us/node/api/dumpprivkey.html
+function dumpPrivKey (address) {
+    return this.$post('dumpprivkey', [address]);
+}
+
+//http://docs.neo.org/en-us/node/api/getaccountstate.html
+function getAccountState (address) {
+    return this.$post('getaccountstate', [address]);
+}
+
+//http://docs.neo.org/en-us/node/api/getapplicationlog.html
+function getApplicationLog (txId, verbose) {
+    return this.$post('getapplicationlog', [txId, verbose ? 1 : 0]);
+}
+
+//http://docs.neo.org/en-us/node/api/getassetstate.html
+function getAssetState (assetId) {
+    return this.$post('getassetstate', [assetId]);
+}
+
+//http://docs.neo.org/en-us/node/api/getbalance.html
 function getBalance (assetId) {
     return this.$post('getbalance', [assetId]);
 }
 
-function getLastBlockHash () {
+//http://docs.neo.org/en-us/node/api/getbestblockhash.html
+function getBestBlockHash () {
     return this.$post('getbestblockhash', []);
 }
 
-function getBlockByHeight$1 (height, verbose) {
-    return this.$post('getblock', [height, verbose ? 1 : 0]);
+//http://docs.neo.org/en-us/node/api/getblock.html
+//http://docs.neo.org/en-us/node/api/getblock2.html
+function getBlock (hashOrIndex, verbose) {
+    return this.$post('getblock', [hashOrIndex, verbose ? 1 : 0]);
 }
 
+//http://docs.neo.org/en-us/node/api/getblockcount.html
 function getBlockCount () {
     return this.$post('getblockcount', []);
 }
 
-function getBlockHashByHeight (height) {
-    return this.$post('getblockhash', [height]);
+//http://docs.neo.org/en-us/node/api/getblockhash.html
+function getBlockHash (index) {
+    return this.$post('getblockhash', [index]);
 }
 
+//http://docs.neo.org/en-us/node/api/getblocksysfee.html
+function getBlockSysFee (index) {
+    return this.$post('getblocksysfee', [index]);
+}
+
+//http://docs.neo.org/en-us/node/api/getconnectioncount.html
 function getConnectionCount () {
     return this.$post('getconnectioncount', []);
 }
 
-function getVersion () {
-    return this.$post('getversion', []);
+//http://docs.neo.org/en-us/node/api/getcontractstate.html
+function getContractState (scriptHash) {
+    return this.$post('getcontractstate', [scriptHash]);
 }
 
+//http://docs.neo.org/en-us/node/api/getnewaddress.html
+function getNewAddress () {
+    return this.$post('getnewaddress', []);
+}
+
+//http://docs.neo.org/en-us/node/api/getrawmempool.html
 function getRawMemPool () {
     return this.$post('getrawmempool', []);
 }
 
+//http://docs.neo.org/en-us/node/api/getrawtransaction.html
 function getRawTransaction (txId, verbose) {
     return this.$post('getrawtransaction', [txId, verbose ? 1 : 0]);
 }
 
-function getTxOut (txId, index) {
-    return this.$post('gettxout', [txId, index]);
+//http://docs.neo.org/en-us/node/api/getstorage.html
+function getStorage (scriptHash, key) {
+    return this.$post('getstorage', [scriptHash, key]);
+}
+
+//http://docs.neo.org/en-us/node/api/gettxout.html
+function getTxOut (txId, n) {
+    return this.$post('gettxout', [txId, n]);
+}
+
+//http://docs.neo.org/en-us/node/api/getpeers.html
+function getPeers () {
+    return this.$post('getpeers', []);
+}
+
+//http://docs.neo.org/en-us/node/api/getversion.html
+function getVersion () {
+    return this.$post('getversion', []);
+}
+
+//http://docs.neo.org/en-us/node/api/invoke.html
+function invoke (scriptHash, params) {
+    return this.$post('invoke', [scriptHash, params]);
+}
+
+//http://docs.neo.org/en-us/node/api/invokefunction.html
+function invokeFunction (scriptHash, operation, params) {
+    return this.$post('invokefunction', [scriptHash, operation, params]);
+}
+
+//http://docs.neo.org/en-us/node/api/invokescript.html
+function invokeScript (script) {
+    return this.$post('invokescript', [script]);
+}
+
+//http://docs.neo.org/en-us/node/api/listaddress.html
+function listAddress () {
+    return this.$post('listaddress', []);
+}
+
+//http://docs.neo.org/en-us/node/api/sendrawtransaction.html
+function  sendRawTransaction(hex) {
+    return this.$post('sendrawtransaction', [hex]);
+}
+
+//http://docs.neo.org/en-us/node/api/sendtoaddress.html
+function  sendToAddress(assetId, address, value, fee) {
+    return this.$post('sendtoaddress', [assetId, address, value, fee ? 1 : 0]);
+}
+
+//http://docs.neo.org/en-us/node/api/sendmany.html
+function  sendMany(outputsArray, fee, changeAddress) {
+    var params = [outputsArray, fee ? 1 : 0];
+    if(changeAddress !== undefined) {
+        params.push(changeAddress);
+    }
+    return this.$post('sendmany', params);
+}
+
+//http://docs.neo.org/en-us/node/api/validateaddress.html
+function  validateAddress(address) {
+    return this.$post('validateaddress', [address]);
 }
 
 var bind = function bind(fn, thisArg) {
